9f4a9560c079b8ff6616a99b652c5717d97be468
z3748 <za3978510@gmail.com>
2024-08-21 18:54:51 +0300

    DIO_config.h

---
+++ 1_MCAL/0_DIO/DIO_config.h
@@ -0,0 +1,14 @@
+/*
+*<<<<<<<<<<<<<<<<<<<<<<< DIO_config.h >>>>>>>>>>>>>>>>>>>>>>>>*
+*
+* AUTHOR : ZIAD AHMED 
+* LAYER : MCAL
+* SWC : DIO/GPIO
+*
+*/
+
+#ifndef DIO_CONFIG_H_
+#define DIO_CONFIG_H_
+
+
+#endif /*DIO_CONFIG_H_*/
