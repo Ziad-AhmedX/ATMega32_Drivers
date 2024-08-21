f9636f68824a574fe5ad4514d8a3542cee24a3f0
z3748 <za3978510@gmail.com>
2024-08-21 19:05:59 +0300

    STD_TYPES.h

---
+++ 3_LIB/STD_TYPES.h
@@ -0,0 +1,25 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<< STD_TYPES_H >>>>>>>>>>>>>>>>>>>>>>>>*
+ *
+ * AUTHOR : ZIAD AHMED
+ * LAYER : LIB
+ *
+ *
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+#define u8 unsigned char
+#define u16 unsigned short int
+#define u32 unsigned long int
+#define u64 unsigned long long int
+#define s8 signed char
+#define s16 signed short int
+#define s32 signed long int
+#define s64 signed long long int
+#define f32 float
+#define f64 double
+#define f128 signed long double
+
+#endif /*STD_TYPES_H_*/
