164bb9e930b6aa46aa33de54cd2c210310e094e5
Ziad_Ahmed <za3978510@gmail.com>
2024-08-28 18:54:56 +0300

    Update DIO_private.h

--- 1_MCAL/0_DIO/DIO_private.h
+++ 1_MCAL/0_DIO/DIO_private.h
@@ -12,9 +12,9 @@
 
 
 /*GROUP A*/
-#define PORTA *((volatile (u8*) 0x3B)
-#define DDRA *((volatile (u8*) 0x3A)
-#define PINA *((volatile (u8*)0x39)
+#define PORTA *((volatile u8*) 0x3B)
+#define DDRA *((volatile u8*) 0x3A)
+#define PINA *((volatile u8*)0x39)
 
 /*GROUP B*/
 #define PORTB *((volatile u8*) 0x38)
@@ -24,7 +24,7 @@
 /*GROUP C*/
 #define PORTC *((volatile u8*)0x35)
 #define DDRC *((volatile u8*)0x34)
-#define PINC *((volatile (u8*)0x33)
+#define PINC *((volatile u8*)0x33)
 
 /*GROUP D*/
 #define PORTD *((volatile u8*) 0x32)
